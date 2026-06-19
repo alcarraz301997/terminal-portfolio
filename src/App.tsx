617f439af471a08e4579e5d8c2141d7dfc627e4f
Junior Alcarraz <guillermo.junior.30@gmail.com>
2026-06-18 23:20:58 -0500

    feat: SEO implementation and skills

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import LanguageSwitcher from "./components/LanguageSwitcher";
 import Terminal from "./components/Terminal";
+import SEO from "./components/SEO";
 
 export default function App() {
   return (
     <div>
+      <SEO />
       <LanguageSwitcher />
       <Terminal/>
     </div>
